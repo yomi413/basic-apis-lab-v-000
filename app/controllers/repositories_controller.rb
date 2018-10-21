8558f4d49f03f21ec000d3bda7e1f4c782ae45d2
Yomaira Escano <yjescano@gmail.com>
2018-10-21 02:39:21 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -9,6 +9,9 @@ class RepositoriesController < ApplicationController
       req.params['client_id'] = 'Iv1.d9e81900426c84b8'
       req.params['client_secret'] = '970c0689f49e82b07757e88aabc1cc274a108d35'
       # req.params['q'] = 'tetris'
+      req.params['name'] = params[:name]
+      req.params['link'] = params[:html_url]
+      req.params['description'] = params[:description]
     end
 
     # body = JSON.parse(@resp.body)
